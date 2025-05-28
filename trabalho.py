3072a21fb7eb2a850d0da4f62da693b1bbe4e5b3
laurabaarbosa <lauraacademico2006@gmail.com>
2025-05-28 12:20:39 -0300

    Update trabalho.py

--- trabalho.py
+++ trabalho.py
@@ -1,1 +1,22 @@
+import streamlit as st
+if 'start_clicked' not in st.session_state:
+    st.session_state.start_clicked = False
+  def reset():
+    st.session_state.start_clicked = False
+    if not st.session_state.start_clicked:
 
+st.set_page_config(page_title="HairBloom", layout="centered")
+if not st.session_state.start_clicked:
+    st.title("🌸 HairBloom - Seu Cronograma Capilar Personalizado")
+    st.write("""
+    Bem Vinda(o) ao HairBloom! Seu Cabelo, Seu Fio, Seu Cronograma.
+    """)
+  pontos = {"hidratação": 0, "nutrição": 0, "reconstrução": 0}
+    if st.button("Começar"):
+        st.session_state.start_clicked = True
+      else:
+    st.header("Primeira Etaoa: Descobrir seu tipo de cabelo!")
+
+textura = st.radio("Qual a textura do seu cabelo?", ["Liso", "Ondulado", "Cacheado", "Crespo"])
+if textura in ["Cacheado", "Crespo"]:
+    pontos["nutrição"] += 1
